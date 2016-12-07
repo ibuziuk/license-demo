db027cd937aab51ef415a66e6a5cd3a0dc3ec5b6
Ilya Buziuk <ibuziuk@redhat.com>
2016-12-07 18:47:55 +0100

    Codehaus mojo plugin setup

--- src/main/java/com/ibuziuk/App.java
+++ src/main/java/com/ibuziuk/App.java
@@ -1,9 +1,14 @@
-/**
- * Copyright (c) 2011 - 2016, Codenvy - initial API and implementation
+/*-
+ * #%L
+ * license-demo
+ * %%
+ * Copyright (C) 2012 - 2016 Codenvy
+ * %%
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ * #L%
  */
 package com.ibuziuk;
 
