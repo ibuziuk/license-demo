f6ea53f420f50099be29781720c12c9ecd5c53c6
Ilya Buziuk <ibuziuk@redhat.com>
2016-12-07 21:55:18 +0100

    Adding multiple contributor

--- src/resources/test.js
+++ src/resources/test.js
@@ -1,7 +1,7 @@
 /*-
  * #%L
  * %%
- * Copyright (C) 2012 - 2016 Codenvy, S.A.
+ * Copyright (C) 2012 - 2016 Codenvy, S.A. Red Hat, SAP, IBM
  * %%
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
