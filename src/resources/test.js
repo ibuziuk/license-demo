37526764d2756f0d9fe1c5b459a1de45581f820e
Ilya Buziuk <ibuziuk@redhat.com>
2016-12-08 20:39:54 +0100

    Swithing back to epl_only_v1 license scheme
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

--- src/resources/test.js
+++ src/resources/test.js
@@ -1,15 +1,12 @@
 /*-
  * #%L
  * %%
- * Copyright (C) 2012 - 2016 Codenvy, S.A. Red Hat, SAP, IBM
+ * Copyright (C) 2012 - 2016 Codenvy, S.A.
  * %%
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors:
- *    Codenvy, S.A.
  * #L%
  */
 function test() {
